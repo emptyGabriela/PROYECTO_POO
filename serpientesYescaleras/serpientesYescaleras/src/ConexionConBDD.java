9e842782944003f294b08c502c7b3c8d8766a37e
Gabriela <101780994+emptyGabriela@users.noreply.github.com>
2023-06-09 09:24:17 -0600

    Conexion con BDD

--- serpientesYescaleras/serpientesYescaleras/src/ConexionConBDD.java
+++ serpientesYescaleras/serpientesYescaleras/src/ConexionConBDD.java
@@ -1,13 +1,16 @@
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class ConexionConBDD {
 
     /**
      * @param args the command line arguments
+     * @throws java.lang.ClassNotFoundException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         Connection conexion = null;
         Statement sentencia = null;
         try{
@@ -20,6 +23,8 @@ public class ConexionConBDD {
         conexion = DriverManager.getConnection(url,"sa","12345678");
         
         
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionConBDD.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
