080402131635126746a9f4f8ca98daf272ce801a
Gabriela <101780994+emptyGabriela@users.noreply.github.com>
2023-06-09 07:48:08 -0600

    Programa Java

---
+++ serpientesYescaleras/serpientesYescaleras/src/VariableHolder.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author david
+ */
+public class VariableHolder {
+  private static boolean halo;
+
+    public static void setHalo(boolean value) {
+        halo = value;
+    }
+
+    public static boolean getHalo() {
+        return halo;
+    }
+}
